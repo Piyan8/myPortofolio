23d0f430969bbb05dd9544c749eaaf9d5e3d8ba5
Piyan8 <bakaart44@gmail.com>
2024-06-20 21:21:22 +0700

    i

--- src/components/Body/Drawing.vue
+++ src/components/Body/Drawing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row my-5">
     <div class="col-12 my-5 p-3 border border rounded">
-      <img src="pixel_art.png" alt="">
+      <img src="pixerl_art.png" alt="">
     </div>
   </div>
 </template>
